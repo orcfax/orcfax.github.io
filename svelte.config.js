1aa27285c793a0b25ff2d9606f28ac87760f247c
Gabriel Chartier <gabriel@chartier.link>
2024-08-14 15:06:11 -0400

    Revert fix

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,6 @@ const config = {
         paths: {
             base: process.env.NODE_ENV === "production" ? process.env.BASE_PATH : "",
         },
-        trailingSlash: "ignore",
     },
 };
 
