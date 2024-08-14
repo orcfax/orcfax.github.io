1b60a3b110446b3f5f79f4eb211757d630e4bf90
Gabriel Chartier <gabriel@chartier.link>
2024-08-14 15:13:15 -0400

    Ignore trailing slash for tos route

--- src/routes/tos/+page.server.ts
+++ src/routes/tos/+page.server.ts
@@ -1,5 +1,7 @@
 import { redirect } from "@sveltejs/kit";
 
+export const trailingSlash = "ignore";
+
 // 302 temporary redirect -> https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/302
 export async function load() {
     throw redirect(302, "https://docs.orcfax.io/terms-of-service");
