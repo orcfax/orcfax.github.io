827a61bcc7ddca8319ef127829c69aad663f4914
Gabriel Chartier <gabriel@chartier.link>
2025-02-06 16:19:55 -0500

    attempt prerender fix

--- src/routes/[rss_feed]/+server.ts
+++ src/routes/[rss_feed]/+server.ts
@@ -2,6 +2,8 @@ import { z } from "zod";
 import { redirect } from "@sveltejs/kit";
 import type { RequestHandler } from "@sveltejs/kit";
 
+export const prerender = false;
+
 export const GET: RequestHandler = async ({ url, params }) => {
     const categoryFilter = url.searchParams.get("category");
     const { category } = RSSFeedParamsSchema.parse({ category: categoryFilter });
