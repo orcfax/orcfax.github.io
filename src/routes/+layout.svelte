7c1bce8993b3ab557b49163f6f0bab1ae30e55dd
Gabriel Chartier <gabriel@chartier.link>
2025-04-08 10:02:15 -0500

    comment out footer and header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import "../app.pcss";
-    import Footer from "$lib/sections/Footer.svelte";
-    import Header from "$lib/sections/Header.svelte";
+    // import Footer from "$lib/sections/Footer.svelte";
+    // import Header from "$lib/sections/Header.svelte";
 
     let { children } = $props();
 </script>
