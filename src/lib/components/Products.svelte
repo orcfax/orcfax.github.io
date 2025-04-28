7aa820df8788dab04226accfe1b90db9b62ad49d
Gabriel Chartier <gabriel@chartier.link>
2025-04-28 08:04:26 -0400

    update products section content

--- src/lib/components/Products.svelte
+++ src/lib/components/Products.svelte
@@ -7,11 +7,9 @@
 <div class="bg-primary">
     <div class="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
         <div class="mx-auto max-w-3xl lg:text-center">
-            <h1 class="mt-2 text-6xl font-bold font-mulish text-secondary">
-                Products and Integrations
-            </h1>
+            <h1 class="mt-2 text-6xl font-bold font-mulish text-secondary">Oracle Solutions</h1>
             <p class="mt-6 text-lg/8 text-secondary font-mulish">
-                The data and proof you need, flexibly delivered.
+                The data and proof you need for your smart contracts.
             </p>
         </div>
 
@@ -19,20 +17,29 @@
             <div class="relative lg:row-span-2">
                 <div class="absolute inset-px rounded-lg bg-card lg:rounded-l-[2rem]"></div>
                 <div
-                    class="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-l-[calc(2rem+1px)]"
+                    class="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-l-[calc(2rem+1px)] border border-slate-400"
                 >
                     <div class="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
                         <p
-                            class="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center"
+                            class="mt-2 text-lg font-semibold tracking-tight text-secondary max-lg:text-center"
                         >
-                            Data Feeds
+                            Oracle Data Feeds
                         </p>
                         <p class="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                            Check out our <a
+                            To learn more about the available feeds, feed types, and feed support
+                            models, refer to our <a
+                                href="https://docs.orcfax.io/feed-overview"
+                                target="_blank"
+                                class="text-accent underline text-sm/6">docs</a
+                            >.
+                        </p>
+                        <p class="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                            You can also view the current active data feeds on our
+                            <a
                                 href="https://explorer.orcfax.io"
+                                target="_blank"
                                 class="text-accent underline text-sm/6">explorer</a
-                            >
-                            to see the current active data feeds.
+                            >.
                         </p>
                     </div>
                     <div
@@ -42,7 +49,7 @@
                             class="absolute inset-x-10 bottom-0 top-10 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 shadow-2xl"
                         >
                             <img
-                                class="size-full object-cover object-top"
+                                class="size-full object-cover object-top p-2"
                                 src="./Explorer.png"
                                 alt=""
                             />
@@ -56,7 +63,7 @@
             <div class="relative max-lg:row-start-1">
                 <div class="absolute inset-px rounded-lg bg-card max-lg:rounded-t-[2rem]"></div>
                 <div
-                    class="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]"
+                    class="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)] border border-slate-400"
                 >
                     <div
                         class="relative flex h-[200px] w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background"
@@ -65,7 +72,7 @@
                     </div>
                     <div class="px-8 sm:px-10 -mt-6 pb-8">
                         <p
-                            class="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center"
+                            class="mt-2 text-lg font-semibold tracking-tight text-secondary max-lg:text-center"
                         >
                             Automatic Heartbeat
                         </p>
@@ -83,7 +90,7 @@
             <div class="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
                 <div class="absolute inset-px rounded-lg bg-card"></div>
                 <div
-                    class="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]"
+                    class="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] border border-slate-400"
                 >
                     <div
                         class="relative flex h-[200px] w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background p-2 pt-8"
@@ -94,7 +101,7 @@
                     </div>
                     <div class="px-8 pt-3 sm:px-10">
                         <p
-                            class="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center"
+                            class="mt-2 text-lg font-semibold tracking-tight text-secondary max-lg:text-center"
                         >
                             On-Demand API <span
                                 class="rounded-full bg-accent/10 px-2 ml-1 py-[0.15rem] text-xs font-semibold text-accent ring-1 ring-inset ring-accent/60"
@@ -117,20 +124,67 @@
                     class="absolute inset-px rounded-lg bg-card max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"
                 ></div>
                 <div
-                    class="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]"
+                    class="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)] border border-slate-400"
                 >
-                    <div class="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
-                        <p
-                            class="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center"
-                        >
-                            Easy Integration With Your Smart Contracts
-                        </p>
-                        <p class="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
-                            Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget sem
-                            sodales gravida.
-                        </p>
+                    <div class="flex flex-col gap-2 px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                        <div>
+                            <p
+                                class="mt-2 text-lg font-semibold tracking-tight text-secondary max-lg:text-center"
+                            >
+                                Integrate With Your Smart Contracts
+                            </p>
+                            <p class="mt-2 max-w-lg text-sm/6 text-secondary max-lg:text-center">
+                                Read our <a
+                                    href="https://docs.orcfax.io/consume"
+                                    target="_blank"
+                                    class="text-accent underline text-sm/6">developer manual</a
+                                >
+                                for integration details and check out our
+                                <a
+                                    href="https://github.com/orcfax/orcfax-examples"
+                                    target="_blank"
+                                    class="text-accent underline text-sm/6">examples</a
+                                >
+                                of demo dApps that utilize Orcfax oracle feeds.
+                            </p>
+                        </div>
+                        <div>
+                            <p
+                                class="mt-6 text-lg font-semibold tracking-tight text-secondary max-lg:text-center"
+                            >
+                                Build With Orcfax
+                            </p>
+                            <p class="mt-2 max-w-lg text-sm/6 text-secondary max-lg:text-center">
+                                We are open to exploring custom oracle solutions for interested and
+                                aligned projects such as:
+                            </p>
+                            <ul class="list-disc list-inside">
+                                <li class="text-sm/6 text-secondary">New feed types</li>
+                                <li class="text-sm/6 text-secondary">Custom data sources</li>
+                                <li class="text-sm/6 text-secondary">Custom data processing</li>
+                                <li class="text-sm/6 text-secondary">Collaborative research</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <p
+                                class="my-4 text-lg font-semibold tracking-tight text-secondary max-lg:text-center"
+                            >
+                                Contact Us For Help & Support
+                            </p>
+                            <div class="flex flex-col justify-center gap-4 mt-2 bg-card">
+                                <Link href="https://discord.gg/orcfax" isNewTab isButton
+                                    >Join Our Discord</Link
+                                >
+                                <Link href="https://x.com/orcfax" isNewTab isButton
+                                    >Message Us On X</Link
+                                >
+                                <Link href="mailto:info@orcfax.io" isNewTab isButton
+                                    >Email Us (info@orcfax.io)</Link
+                                >
+                            </div>
+                        </div>
                     </div>
-                    <div class="relative min-h-[30rem] w-full">
+                    <!-- <div class="relative min-h-[30rem] w-full">
                         <div
                             class="absolute bottom-0 left-10 right-0 top-10 overflow-hidden rounded-tl-xl bg-secondary shadow-2xl"
                         >
@@ -147,11 +201,7 @@
                                 <code class="text-sm"> Code example here </code>
                             </div>
                         </div>
-                    </div>
-                    <div class="flex justify-center gap-4 mt-2 bg-card">
-                        <Link href="https://docs.orcfax.io" isNewTab>Read Docs</Link>
-                        <Link href="https://discord.gg/orcfax" isNewTab>Get Help</Link>
-                    </div>
+                    </div> -->
                 </div>
                 <div
                     class="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"
