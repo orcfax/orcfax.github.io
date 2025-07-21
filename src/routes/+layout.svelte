e1bb8c8c1eab1fee79ad22c694f4d74e084ee888
Gabriel Chartier <gabriel@chartier.link>
2025-07-21 13:46:50 -0500

    add umami analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,14 @@
     let { children } = $props();
 </script>
 
+<!-- Setup Privacy-preserving Umami Analytics -->
+<svelte:head>
+    <script
+        async
+        defer
+        data-website-id="2d656c7d-b41a-4e19-934c-d7c9d91d784d"
+        src="https://analytics.server.orcfax.io/umami.js"
+    ></script>
+</svelte:head>
+
 {@render children()}
