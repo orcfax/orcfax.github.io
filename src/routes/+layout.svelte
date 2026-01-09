1cd7c5a023147fdff3f19f5974e53e02a2dff721
Gabriel Chartier <gabriel@chartier.link>
2026-01-09 12:37:20 -0600

    temporarily disable analytics since it is slowing down site, add robots.txt

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-    import "../app.pcss";
+  import "../app.pcss";
 
-    let { children } = $props();
+  let { children } = $props();
 </script>
 
 <!-- Setup Privacy-preserving Umami Analytics -->
-<svelte:head>
+<!-- <svelte:head>
     <script
         defer
         src="https://analytics.server.orcfax.io/script.js"
         data-website-id="8cf70d2e-1297-4f17-bc58-55d1a296e55a"
     ></script>
-</svelte:head>
+</svelte:head> -->
 
 {@render children()}
